e1d69ee6ea0b1a47f6443b3ccca0612794aa707b
Valeriy Dyakonov <Valeriy.Dyakonov@waveaccess.ru>
2022-04-19 21:49:29 +0400

    added bottom navigation menu and fragments

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,18 +1,43 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.mainFragment.CalendarFragment
+import com.example.myapplication.mainFragment.MapFragment
+import com.example.myapplication.mainFragment.NotesListFragment
+import com.example.myapplication.mainFragment.ProfileFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNav()
+        setFragment(R.id.mainFragmentLayout, NotesListFragment.newInstance())
     }
 
-    fun onButtonClick(view: View) {
+    private fun initBottomNav() {
+        val bNav = binding.bNav
+        bNav.selectedItemId = R.id.notes
+        bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.notes -> setFragment(R.id.mainFragmentLayout, NotesListFragment.newInstance())
+                R.id.calendar -> setFragment(R.id.mainFragmentLayout, CalendarFragment.newInstance())
+                R.id.map -> setFragment(R.id.mainFragmentLayout, MapFragment.newInstance())
+                R.id.profile -> setFragment(R.id.mainFragmentLayout, ProfileFragment.newInstance())
+            }
+            true
+        }
+    }
 
+    private fun setFragment(frameLayout: Int, fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(frameLayout, fragment)
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/myapplication/mainFragment/CalendarFragment.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.mainFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.myapplication.R
+
+class CalendarFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = CalendarFragment()
+    }
+}
