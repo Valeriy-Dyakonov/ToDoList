6a9cab3f8405bb41e350852e08a61c9aa62df690
Valeriy Dyakonov <Valeriy.Dyakonov@waveaccess.ru>
2022-04-19 22:30:59 +0400

    added note list recycler view

---
+++ app/src/main/java/com/example/myapplication/adapter/NoteAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import com.example.myapplication.databinding.NoteItemBinding
+import com.example.myapplication.model.Note
+
+class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteHolder>() {
+    val noteList = ArrayList<Note>()
+
+    class NoteHolder(item: View) : RecyclerView.ViewHolder(item) {
+        var binding = NoteItemBinding.bind(item)
+        fun bind(note: Note) {
+            binding.noteTextView.text = note.name
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.note_item, parent, false)
+        return NoteHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NoteHolder, position: Int) {
+        holder.bind(noteList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return noteList.size
+    }
+
+    fun addNote(note: Note) {
+        noteList.add(note)
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/example/myapplication/mainFragment/NotesListFragment.kt
+++ app/src/main/java/com/example/myapplication/mainFragment/NotesListFragment.kt
@@ -4,19 +4,37 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.myapplication.R
+import com.example.myapplication.adapter.NoteAdapter
+import com.example.myapplication.databinding.FragmentNotesListBinding
+import com.example.myapplication.model.Note
 
 class NotesListFragment : Fragment() {
+    lateinit var binding: FragmentNotesListBinding
+    private val adapter = NoteAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_notes_list, container, false)
+    ): View {
+        binding = FragmentNotesListBinding.inflate(inflater)
+        initRecyclerView()
+        return binding.root
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = NotesListFragment()
+    }
+
+    private fun initRecyclerView() {
+        binding.apply {
+            noteRcView.layoutManager = LinearLayoutManager(requireContext())
+            noteRcView.adapter = adapter
+
+        }
     }
 }
