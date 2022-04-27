35673b0346de4a1b11c7b9d288201866699dadee
Valeriy Dyakonov <Valeriy.Dyakonov@waveaccess.ru>
2022-04-28 01:50:59 +0400

    added test

--- app/src/test/java/com/example/myapplication/utils/DateUtilsTest.kt
+++ app/src/test/java/com/example/myapplication/utils/DateUtilsTest.kt
@@ -6,7 +6,7 @@ import java.util.*
 
 class DateUtilsTest {
     @Test
-    fun testToString() {
+    fun testDateUtils_1() {
         val instance = Calendar.getInstance()
         val day = instance.get(Calendar.DAY_OF_MONTH)
         val month = instance.get(Calendar.MONTH) + 1
@@ -25,9 +25,45 @@ class DateUtilsTest {
 
         val value = "$dateString $timeString"
         Assertions.assertEquals(value, toString)
+    }
+
+    @Test
+    fun testDateUtils_2() {
+        val instance = Calendar.getInstance()
+        val day = instance.get(Calendar.DAY_OF_MONTH)
+        val month = instance.get(Calendar.MONTH) + 1
+        val year = instance.get(Calendar.YEAR)
+        val HOUR = instance.get(Calendar.HOUR_OF_DAY)
+        val MINUTE = instance.get(Calendar.MINUTE)
+        val time = DateUtils.toDate(DateUtils.toString(instance.time, DateUtils.DATE_WITH_TIME), DateUtils.DATE_WITH_TIME)
+
+        val dateString = (if (day < 10) "0$day" else day.toString()) +
+                "/" + (if (month < 10) "0$month" else month.toString()) +
+                "/" + year.toString()
+        val timeString =
+            (if (HOUR < 10) "0$HOUR" else HOUR.toString()) + ":" + (if (MINUTE < 10) "0$MINUTE" else MINUTE.toString())
+
+        val value = "$dateString $timeString"
 
         val toDate = DateUtils.toDate(value, DateUtils.DATE_WITH_TIME)
         Assertions.assertEquals(time, toDate)
+    }
+
+    @Test
+    fun testDateUtils_3() {
+        val instance = Calendar.getInstance()
+        val day = instance.get(Calendar.DAY_OF_MONTH)
+        val month = instance.get(Calendar.MONTH) + 1
+        val year = instance.get(Calendar.YEAR)
+        val HOUR = instance.get(Calendar.HOUR_OF_DAY)
+        val MINUTE = instance.get(Calendar.MINUTE)
+        val time = DateUtils.toDate(DateUtils.toString(instance.time, DateUtils.DATE_WITH_TIME), DateUtils.DATE_WITH_TIME)
+
+        val dateString = (if (day < 10) "0$day" else day.toString()) +
+                "/" + (if (month < 10) "0$month" else month.toString()) +
+                "/" + year.toString()
+        val timeString =
+            (if (HOUR < 10) "0$HOUR" else HOUR.toString()) + ":" + (if (MINUTE < 10) "0$MINUTE" else MINUTE.toString())
 
         val concatDateAndTime = DateUtils.concatDateAndTime(dateString, timeString)
         Assertions.assertEquals(time, concatDateAndTime)
